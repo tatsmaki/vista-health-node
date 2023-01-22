51174de79723b1e1c7b15461ea8482b40cfeff33
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2023-01-22 20:48:34 +0100

    fix: add PORT to env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function start() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3002);
+  await app.listen(process.env.PORT || 3002);
 }
 start();
